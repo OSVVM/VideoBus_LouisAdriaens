a58356c14c009204e3b3eb4c277850ffa8a97991
Jim Lewis <jim@synthworks.com>
2025-06-19 12:39:33 -0700

    Updated TranscriptOpen

--- testbench/TbVideoBus_SendGet.vhd
+++ testbench/TbVideoBus_SendGet.vhd
@@ -36,7 +36,7 @@ begin
 
         -- Wait for testbench initialization 
         wait for 0 ns ;  wait for 0 ns;
-        TranscriptOpen(OSVVM_RESULTS_DIR & "TbVideoBus_SendGet.txt");
+        TranscriptOpen ;
         SetTranscriptMirror(TRUE); 
 
         -- Wait for Design Reset
